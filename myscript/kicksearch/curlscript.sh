13d95a7f5030d3b69b0ce8a8cbcbcf1fa5e3b53f
Hex-Tron <xredbro@gmail.com>
2022-03-14 20:10:40 +0530

    kick complete1

--- myscript/kicksearch/curlscript.sh
+++ myscript/kicksearch/curlscript.sh
@@ -41,6 +41,7 @@ Top Doc :top-100-documentries
 
 '
 
+browser="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
 function search_FUNCTION() {
 
 
@@ -49,44 +50,43 @@ function search_FUNCTION() {
   local val1=$(echo '' | $DMENU -p 'Enter the query:' )
   local val2=$(/home/hextron/Tools/project/kicksearch/space_to_20/spaceto20 "$val1")
 
-  browser="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
   function curlquery() {
-  echo "Loading ..." | $DMENU &
-  PID=$!
-	  curl  -x 'socks5://localhost:9050' -A "$browser" "$Baseurl/$val2/$((page++))" > $HOME/.cache/torrent/htmlfile.html
+    echo "Loading ..." | $DMENU &
+    PID=$!
+    curl  -x 'socks5://localhost:9050' -A "$browser" "$Baseurl/$val2/$((page++))" > $HOME/.cache/torrent/htmlfile.html
 
-  $HOME/Tools/project/kicksearch/kick $HOME/.cache/torrent/htmlfile.html> $HOME/.cache/torrent/formated.txt  
-  kill $PID
-  new=$(cat $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $1 " -: " $3 " -: " $4  }' )
-  new1=$(echo -e "$new\nnext" | $DMENU -p "$val1")
-}
+    $HOME/Tools/project/kicksearch/kick $HOME/.cache/torrent/htmlfile.html> $HOME/.cache/torrent/formated.txt  
+    kill $PID
+    new=$(cat $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $1 " -: " $3 " -: " $4  }' )
+    new1=$(echo -e "$new\nnext" | $DMENU -p "$val1")
+  }
 while :
 do
-	  curlquery
+  curlquery
   if [[ $new1 != "next" ]]; then 
-  break
+    break
   fi
 done
-  local newchange=$(echo -e "$new1" | awk -F ' -: ' '{print $1}')
-  local box=( "$newchange" )
-  local changed=$(echo -e "$box" | awk -F '[' '{print $1}') # error when  square bracket appear 
-  local changed1=("$changed")
-  if [[ -z "$changed1" ]]; then
-    return
-  fi
-  local final=$(grep "${changed1[@]}" $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $2}')
-  # + local final=/pegasproductions-22-01-18-luna-temptress-my-step-sis-tiny-pussy-xxx-720p-hevc-x265-prt-xvx-t5129078.html
+local newchange=$(echo -e "$new1" | awk -F ' -: ' '{print $1}')
+local box=( "$newchange" )
+local changed=$(echo -e "$box" | awk -F '[' '{print $1}') # error when  square bracket appear 
+local changed1=("$changed")
+if [[ -z "$changed1" ]]; then
+  return
+fi
+local final=$(grep "${changed1[@]}" $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $2}')
+# + local final=/pegasproductions-22-01-18-luna-temptress-my-step-sis-tiny-pussy-xxx-720p-hevc-x265-prt-xvx-t5129078.html
 
-  echo "Loading ..." | $DMENU &
-  PID2=$!
-  curl -x 'socks5://localhost:9050' -A "$browser" "$mainurl$final" > $HOME/.cache/torrent/checkfile.html
-  kill $PID2
-  magnetlink=$(/home/hextron/Tools/project/kicksearch/regexmatch/regexm $HOME/.cache/torrent/checkfile.html)
+echo "Loading ..." | $DMENU &
+PID2=$!
+curl -x 'socks5://localhost:9050' -A "$browser" "$mainurl$final" > $HOME/.cache/torrent/checkfile.html
+kill $PID2
+magnetlink=$(/home/hextron/Tools/project/kicksearch/regexmatch/regexm $HOME/.cache/torrent/checkfile.html)
 
 
-  if [[ -n "$magnetlink" ]]; then
-    qbittorrent "$magnetlink" &
-  fi
+if [[ -n "$magnetlink" ]]; then
+  qbittorrent "$magnetlink" &
+fi
 }
 
 
@@ -95,35 +95,61 @@ if [[ $value =~ "Search" ]]; then
   search_FUNCTION
   exit 0 
 fi
+
+
 Newlink=$(echo -e "$Nice"  | grep "$value" | head -n 1 | awk -F ':' '{print $2}')
-
-  browser="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+function curlquery1() {
   echo "Loading ..." | $DMENU &
-  PID3=$!
-	  curl  -x 'socks5://localhost:9050' -A "$browser" "$Baseurl/$Newlink" > $HOME/.cache/torrent/htmlfile.html
-
-  $HOME/Tools/project/kicksearch/kick $HOME/.cache/torrent/htmlfile.html> $HOME/.cache/torrent/formated.txt  
-  kill $PID3
+  PID=$!
+  curl  -x 'socks5://localhost:9050' -A "$browser" "$Baseurl/$Newlink/$((page++))" > $HOME/.cache/torrent/htmlfile.html
+  $HOME/Tools/project/kicksearch/kick $HOME/.cache/torrent/htmlfile.html > $HOME/.cache/torrent/formated.txt  
+  kill $PID
   new=$(cat $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $1 " -: " $3 " -: " $4  }' )
   new1=$(echo -e "$new\nnext" | $DMENU -p "$val1")
-
-   newchange=$(echo -e "$new1" | awk -F ' -: ' '{print $1}')
-   box=( "$newchange" )
-   changed=$(echo -e "$box" | awk -F '[' '{print $1}') # error when  square bracket appear 
-   changed1=("$changed")
-  if [[ -z "$changed1" ]]; then
-    return
+}
+while :
+do
+  curlquery1
+  if [[ $new1 != "next" ]]; then 
+    break
   fi
-   final=$(grep "${changed1[@]}" $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $2}')
-  # +  final=/pegasproductions-22-01-18-luna-temptress-my-step-sis-tiny-pussy-xxx-720p-hevc-x265-prt-xvx-t5129078.html
+done
+#echo "Loading ..." | $DMENU &
+#PID3=$!
+#curl  -x 'socks5://localhost:9050' -A "$browser" "$Baseurl/$Newlink" > $HOME/.cache/torrent/htmlfile.html
+#$HOME/Tools/project/kicksearch/kick $HOME/.cache/torrent/htmlfile.html> $HOME/.cache/torrent/formated.txt  
+#kill $PID3
+#new=$(cat $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $1 " -: " $3 " -: " $4  }' )
+#new1=$(echo -e "$new\nnext" | $DMENU -p "$val1")
 
-  echo "Loading ..." | $DMENU &
-  PID2=$!
-  curl -x 'socks5://localhost:9050' -A "$browser" "$Baseurl$final" > $HOME/.cache/torrent/checkfile.html
-  kill $PID2
-  magnetlink=$(/home/hextron/Tools/project/kicksearch/regexmatch/regexm $HOME/.cache/torrent/checkfile.html)
 
 
-  if [[ -n "$magnetlink" ]]; then
-    qbittorrent "$magnetlink" &
-  fi
+
+
+
+
+
+
+
+
+
+newchange=$(echo -e "$new1" | awk -F ' -: ' '{print $1}')
+box=( "$newchange" )
+changed=$(echo -e "$box" | awk -F '[' '{print $1}') # error when  square bracket appear 
+changed1=("$changed")
+if [[ -z "$changed1" ]]; then
+  return
+fi
+final=$(grep "${changed1[@]}" $HOME/.cache/torrent/formated.txt | awk -F ':-:' '{print $2}')
+# +  final=/pegasproductions-22-01-18-luna-temptress-my-step-sis-tiny-pussy-xxx-720p-hevc-x265-prt-xvx-t5129078.html
+
+echo "Loading ..." | $DMENU &
+PID2=$!
+curl -x 'socks5://localhost:9050' -A "$browser" "$Baseurl$final" > $HOME/.cache/torrent/checkfile.html
+kill $PID2
+magnetlink=$(/home/hextron/Tools/project/kicksearch/regexmatch/regexm $HOME/.cache/torrent/checkfile.html)
+
+
+if [[ -n "$magnetlink" ]]; then
+  qbittorrent "$magnetlink" &
+fi
